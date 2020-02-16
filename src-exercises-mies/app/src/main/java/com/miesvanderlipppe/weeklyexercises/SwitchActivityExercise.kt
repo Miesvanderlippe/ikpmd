4e16732905f2003ca190e7dc2f3484e999f1cfd1
Mies van der Lippe <m.vanderlippe@woonz.nl>
2020-02-16 18:26:11 +0100

    Week 1 exercise, switch layout using button

---
+++ src-exercises-mies/app/src/main/java/com/miesvanderlipppe/weeklyexercises/SwitchActivityExercise.kt
@@ -0,0 +1,12 @@
+package com.miesvanderlipppe.weeklyexercises
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SwitchActivityExercise : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_switch_exercise)
+    }
+}

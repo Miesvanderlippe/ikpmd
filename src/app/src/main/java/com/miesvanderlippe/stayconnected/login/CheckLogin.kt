c311ad33b467355fc56620849379ed36070cc3c3
roelo34 <roeldeking@gmail.com>
2020-04-07 22:16:29 +0200

    Class toegevoegd waarmee de login status gecheckt kan worden

---
+++ src/app/src/main/java/com/miesvanderlippe/stayconnected/login/CheckLogin.kt
@@ -0,0 +1,20 @@
+package com.miesvanderlippe.stayconnected.login
+
+import android.content.Context
+
+class CheckLogin (context: Context) {
+
+    val PREFERENCE_NAME = "token"
+    val PREFERENCE_USER_KEY = "Key"
+
+    val userData = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
+
+    fun getUserKey(): String? {
+        return userData.getString(PREFERENCE_USER_KEY, "None")
+    }
+
+    fun checkLogin() : Boolean {
+        val loggedIn = getUserKey()
+        return loggedIn != "None"
+    }
+}

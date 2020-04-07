3e8fdb308b76f8e698a1dd5d48ae3a21ff9021dc
roelo34 <roeldeking@gmail.com>
2020-04-07 17:49:46 +0200

    Class voor User model en API data model

--- src/app/src/main/java/com/miesvanderlippe/stayconnected/modal/User.kt
+++ src/app/src/main/java/com/miesvanderlippe/stayconnected/modal/User.kt
@@ -1,4 +1,6 @@
 package com.miesvanderlippe.stayconnected.modal
 
-class User {
-}
+//model class
+data class User(val email: String, val key: String, val password: String)
+
+data class apiData (val success: String, val token: String)

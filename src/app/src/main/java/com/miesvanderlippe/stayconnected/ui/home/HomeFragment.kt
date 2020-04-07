8926953f0bea32bec3288e370d039fa091425b0e
roelo34 <roeldeking@gmail.com>
2020-04-07 22:19:12 +0200

    FetchKey wordt nu succesvol aangeroepen met bijbehorende data wanneer op 'Inloggen' geklikt word

--- src/app/src/main/java/com/miesvanderlippe/stayconnected/ui/home/HomeFragment.kt
+++ src/app/src/main/java/com/miesvanderlippe/stayconnected/ui/home/HomeFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.miesvanderlippe.stayconnected.R
+import com.miesvanderlippe.stayconnected.login.FetchKey
+import com.miesvanderlippe.stayconnected.login.InputValidation
+import com.miesvanderlippe.stayconnected.modal.User
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,4 +28,17 @@ class HomeFragment : Fragment() {
 
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        appCompatButtonLogin.setOnClickListener {
+            // WIP
+            val validate = InputValidation(view.context)
+            val user = User(textInputEditTextEmail.text.toString(), "None", textInputEditTextPassword.text.toString())
+            FetchKey(view.context, user)
+        }
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
 }

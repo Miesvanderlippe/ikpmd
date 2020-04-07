b1c089a22cd4b94d650d9a6a6e8d61012f286b44
roelo34 <roeldeking@gmail.com>
2020-04-07 17:52:00 +0200

    Class voor het opslaan van email:key in de 'SharedPreferences'

--- src/app/src/main/java/com/miesvanderlippe/stayconnected/storage/DataStorage.kt
+++ src/app/src/main/java/com/miesvanderlippe/stayconnected/storage/DataStorage.kt
@@ -1,4 +1,28 @@
 package com.miesvanderlippe.stayconnected.storage
 
-class DataStorage {
+import android.content.Context
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.GsonBuilder
+
+class DataStorage (val context: Context) {
+
+    val PREFERENCE_NAME = "temp@replace.this"
+    val PREFERENCE_USER_KEY = "testing_value_for_key"
+
+    val userData = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
+
+    fun getUserKey(): String? {
+        return userData.getString(PREFERENCE_USER_KEY, "None")
+    }
+
+    fun setUserKey(userkey: String) {
+        val key = ""
+        val editor = userData.edit()
+        editor.putString(PREFERENCE_USER_KEY, key)
+        editor.apply()
+    }
+
 }

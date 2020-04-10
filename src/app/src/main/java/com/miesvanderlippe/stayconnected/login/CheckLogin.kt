ba36092335a61632e399697b419e076fbcb1ca7b
roelo34 <roeldeking@gmail.com>
2020-04-10 18:38:07 +0200

    Kleine veranderingen in het verwerken van de gegeven data en API data

--- src/app/src/main/java/com/miesvanderlippe/stayconnected/login/CheckLogin.kt
+++ src/app/src/main/java/com/miesvanderlippe/stayconnected/login/CheckLogin.kt
@@ -8,6 +8,7 @@ class CheckLogin (context: Context) {
     val PREFERENCE_USER_KEY = "Key"
     val PREFERENCE_USER_EMAIL = "Email"
     val PREFERENCE_USER_NAME = "Name"
+    val PREFERENCE_USER_ISADMIN = "isAdmin"
 
     val userData = context.getSharedPreferences(PREFERENCE_NAME, Context.MODE_PRIVATE)
 
@@ -27,9 +28,11 @@ class CheckLogin (context: Context) {
         return userData.getString(PREFERENCE_USER_KEY, "None").toString()
     }
 
+    fun getAdminStatus(): Boolean {
+        return userData.getString(PREFERENCE_USER_ISADMIN, "None").toString() == "true"
+    }
 
     fun checkLogin() : Boolean {
-        val loggedIn = getUserKey()
-        return loggedIn != "None"
+        return getUserKey() != "None"
     }
 }
